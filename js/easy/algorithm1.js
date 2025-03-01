6b2d6c47270faa7d95fdb59c73f7ab390d258d17
Nigar <haciyeva.nigar2004@gmail.com>
2025-03-01 18:20:36 +0400

    user validation

--- js/easy/algorithm1.js
+++ js/easy/algorithm1.js
@@ -1,7 +1,7 @@
 //user validation
  const userValidation=(str)=>{
 if(  
-      str.length>=4 && str.length<=25 && (/[a-z A-Z]/).test(str.slice(0,1)/*str.charAt(0)*/)&& (/^\w+$/).test(str)){
+      str.length>=4 && str.length<=25 && (/[a-z A-Z]/).test(str.slice(0,1)/*str.charAt(0)*/)&& (/^\w+$/).test(str) && (/[a-zA-Z0-9]/).test(str.slice(-1)/*str.charAt(str.length-1)*/)){
 return true
 }else{
     return false
@@ -26,3 +26,4 @@ if the username is valid then your program should return the string true
 //^ bu stringin basini gosterir,yeni uygunlug stringin basindan baslam9alidir
 //\w+ - \w bir sözdən simvolu ifadə edir, yəni hərf, rəqəm və alt xət (underscore) (_). + işarəsi isə, bir və ya daha çox bu cür simvolların olmasını tələb edir. Yəni, burada bir və ya daha çox hərf, rəqəm və ya alt xətt olmalıdır.
 //$ - Bu, stringin sonunu göstərir. Yəni, uyğunluq stringin sonuna qədər olmalıdır.
+//.slice(-1) sozun uzunlugu ne qeder olur olsun son deyeri qaytarir
