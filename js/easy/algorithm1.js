72b0fe735630a71939486af668c3694bc05a62b2
Nigar <haciyeva.nigar2004@gmail.com>
2025-03-01 18:00:19 +0400

    Algorithm 1

---
+++ js/easy/algorithm1.js
@@ -0,0 +1,28 @@
+//user validation
+ const userValidation=(str)=>{
+if(  
+      str.length>=4 && str.length<=25 && (/[a-z A-Z]/).test(str.slice(0,1)/*str.charAt(0)*/)&& (/^\w+$/).test(str)){
+return true
+}else{
+    return false
+}
+}
+console.log(userValidation("u__hello__world1d23"))
+/*Rules
+1. the username is between 4 and 25 characters
+2. it must start with a letter
+3. it can only contain letters, numbers and underscores characters
+4. it cannot end with an underscore character
+
+if the username is valid then your program should return the string true
+*/
+
+// AND Operator
+// .length property
+
+// Regular expression
+// .charAt() or .slice()
+// .test()
+//^ bu stringin basini gosterir,yeni uygunlug stringin basindan baslam9alidir
+//\w+ - \w bir sözdən simvolu ifadə edir, yəni hərf, rəqəm və alt xət (underscore) (_). + işarəsi isə, bir və ya daha çox bu cür simvolların olmasını tələb edir. Yəni, burada bir və ya daha çox hərf, rəqəm və ya alt xətt olmalıdır.
+//$ - Bu, stringin sonunu göstərir. Yəni, uyğunluq stringin sonuna qədər olmalıdır.
---
+++ js/easy/index.js
@@ -0,0 +1,8 @@
+let arr = [7,8,4,6,0,3,34];
+let arr = [-3,-1,-19];
+let max = arr[0];
+for (let index = 0; index < arr.length; index++) {
+    if (arr[index] > max){
+        max = arr[index];
+    }
+}
