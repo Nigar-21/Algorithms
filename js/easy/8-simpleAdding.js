9b90571fd31419e2e07c92397fdea4b717a47824
Nigar <haciyeva.nigar2004@gmail.com>
2025-03-02 23:19:05 +0400

    simple adding

---
+++ js/easy/8-simpleAdding.js
@@ -0,0 +1,11 @@
+const simpleAdding=(num)=>{
+    let result=0
+for(var i=0;i<=num;i++){
+    result+=i
+}
+return result
+}
+console.log(simpleAdding(7));
+
+
+/*verilen tam reqeme qeder olan deyerlerin toplanmasi(verilen deyer daxil)*/
