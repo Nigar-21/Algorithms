5ac847b39047ec6178cfbb1111a5152b7a32f9f6
Nigar <haciyeva.nigar2004@gmail.com>
2025-03-03 13:31:17 +0400

    number check

---
+++ js/easy/11-numberCheck.js
@@ -0,0 +1,17 @@
+const checkNumber=(num1,num2)=>{
+if(num1===num2){
+    return -1
+}else if(num2>num1){
+return true
+}else{
+    return false
+}
+}
+console.log(checkNumber(7,8));
+
+
+/*
+have the function checkNumber(num1,num2) take parameters being passed and return the string true if num2 is greater than num1
+otherwise return the string false
+if the parameter values are equal to each other then return the string-1
+*/
