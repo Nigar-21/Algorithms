97832da05ffdd2ed67dbfb4a282c85f51912a3d4
Nigar <haciyeva.nigar2004@gmail.com>
2025-03-17 13:22:18 +0400

    word count

--- js/easy/19-insertPosition.js
+++ js/easy/19-insertPosition.js
@@ -4,7 +4,7 @@ const insertPosition = (nums, target) => {
   if (nums.includes(target)) {
     return nums.indexOf(target);
   } else {for (let index = 0; index < nums.length; index++) {
-    if (target<nums[index]) {
+    if (target<=nums[index]) {
         return index;
     }
 }
---
+++ js/easy/20-wordCount.js
@@ -0,0 +1,5 @@
+const wordCount = (str)=>{
+return str.trim().split(" ").length
+
+}
+console.log(wordCount("Never eat shredded wheat"));
